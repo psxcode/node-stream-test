1b1bee1e2f2ae1c2453939b2804a7d1e5107b37a
alex.feinstein <alex.feinstein@klarna.com>
2018-09-24 09:33:45 +0200

    refactor producer

--- src/producer.ts
+++ src/producer.ts
@@ -1,4 +1,5 @@
 import WritableStream = NodeJS.WritableStream
+import { iterate } from 'iterama'
 
 export type ProducerOptions = {
   log: typeof console.log
@@ -6,53 +7,53 @@ export type ProducerOptions = {
 }
 
 const producer = ({ eager, log }: ProducerOptions) =>
-  <T> (iterator: Iterator<T>, maxLength = 0) =>
-    (stream: WritableStream) => {
-      let i = 0
-      const eagerWriter = () => {
-        log('eager writing begin at %d', i)
-        while (writeChunk(iterator.next())) {
-          ++i
+  <T> (stream: WritableStream, iterable: Iterable<T>) => {
+    const it = iterate(iterable)
+    let i = 0
+    const eagerWriter = () => {
+      log('eager writing begin at %d', i)
+      while (writeChunk(it.next())) {
+        ++i
+      }
+      log('eager writing done at %d', i - 1)
+    }
+    const lazyWriter = () => {
+      log('lazy writing begin at %d', i)
+      writeChunk(it.next(), lazyWriter)
+      log('lazy writing done at %d', i - 1)
+    }
+    const writeChunk = (iteratorResult: IteratorResult<T>, cb?: () => void): boolean => {
+      if (iteratorResult.done) {
+        log('ending %d', i)
+        stream.end()
+        return false
+      } else {
+        log('writing %d', i)
+        const backpressure = stream.write(iteratorResult.value as any, cb)
+        if (!backpressure) {
+          log('backpressure at %d', i)
         }
-        log('eager writing done at %d', i - 1)
-      }
-      const lazyWriter = () => {
-        log('lazy writing begin at %d', i)
-        writeChunk(iterator.next(), lazyWriter)
-        log('lazy writing done at %d', i - 1)
-      }
-      const writeChunk = (iteratorResult: IteratorResult<T>, cb?: () => void): boolean => {
-        if (iteratorResult.done || (maxLength > 0 && i >= maxLength)) {
-          log('ending %d', i)
-          stream.end()
-          return false
-        } else {
-          log('writing %d', i)
-          const backpressure = stream.write(iteratorResult.value as any, cb)
-          if (!backpressure) {
-            log('backpressure at %d', i)
-          }
-          return backpressure
-        }
-      }
-      const onDrainEvent = () => {
-        log('received \'drain\' event %d', i)
-        eager ? eagerWriter() : lazyWriter()
-      }
-      const unsubscribe = () => {
-        log('unsubscribe')
-        stream.removeListener('drain', onDrainEvent)
-        stream.removeListener('finish', unsubscribe)
-      }
-
-      return () => {
-        log('producer subscribe')
-        stream.on('drain', onDrainEvent)
-        stream.once('finish', unsubscribe)
-        /* drain event could already be emitted, try to write once */
-        eager ? eagerWriter() : lazyWriter()
-        return unsubscribe
+        return backpressure
       }
     }
+    const onDrainEvent = () => {
+      log('received \'drain\' event %d', i)
+      eager ? eagerWriter() : lazyWriter()
+    }
+    const unsubscribe = () => {
+      log('unsubscribe')
+      stream.removeListener('drain', onDrainEvent)
+      stream.removeListener('finish', unsubscribe)
+    }
+
+    return () => {
+      log('producer subscribe')
+      stream.on('drain', onDrainEvent)
+      stream.once('finish', unsubscribe)
+      /* drain event could already be emitted, try to write once */
+      eager ? eagerWriter() : lazyWriter()
+      return unsubscribe
+    }
+  }
 
 export default producer
