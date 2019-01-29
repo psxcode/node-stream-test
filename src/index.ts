dd3257e074197d50693ad9387c1c99b63ce71dea
psxcode <psxcode@gmail.com>
2019-01-29 13:51:35 +0100

    fix entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as readable } from './readable'
 export { default as writable } from './writable'
 export { default as pushConsumer } from './push-consumer'
-export { default as consumer } from './pull-consumer'
+export { default as pullConsumer } from './pull-consumer'
 export { default as producer } from './producer'
